c3398851ba82109aa2ade48341bf1191452f9d04
chouhanvikash <96488608+chouhanvikash@users.noreply.github.com>
2025-09-18 08:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let isbn = document.getElementById("isbn");
+let btn = document.getElementById("submit");
+let tableBody = document.getElementById("book-list");
 
-    let title = document.getElementById("title");
-    let author = document.getElementById("author");
-    let isbn = document.getElementById("isbn");
-    let btn = document.getElementById("submit");
-    let table = document.getElementById("table").getElementsByTagName('tbody')[0];
-table.style.backgroundColor="gray";
-    btn.addEventListener("click", add);
+btn.addEventListener("click", add);
 
-    function add(e) {
-      e.preventDefault();
+function add(e) {
+  e.preventDefault();
 
-      let row = table.insertRow();
-      row.insertCell(0).textContent = title.value;
-      row.insertCell(1).textContent = author.value;
-      row.insertCell(2).textContent = isbn.value;
+  let row = tableBody.insertRow();
+  row.insertCell(0).textContent = title.value;
+  row.insertCell(1).textContent = author.value;
+  row.insertCell(2).textContent = isbn.value;
 
-      // clear inputs
-      title.value = "";
-      author.value = "";
-      isbn.value = "";
-				let deleteCell = row.insertCell(3);
-let btn = document.createElement("button");
-btn.textContent = "x";
-		btn.style.backgroundColor = "red"; 
-btn.addEventListener("click", () => row.remove());
-deleteCell.appendChild(btn);
+  // delete button
+  let deleteCell = row.insertCell(3);
+  let delBtn = document.createElement("button");
+  delBtn.textContent = "X";
+  delBtn.className = "delete";
+  delBtn.addEventListener("click", () => row.remove());
+  deleteCell.appendChild(delBtn);
 
-    }
+  // clear inputs
+  title.value = "";
+  author.value = "";
+  isbn.value = "";
+}
